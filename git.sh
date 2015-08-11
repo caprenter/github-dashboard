65b51e4d67821532138221bc4d9a981a427b486f
David Carpenter <caprenter@gmail.com>
2015-08-11 16:34:55 +0100

    Creates a version for Open Data Services just using the Git stuff
    Rips much of the application out of the code base to just use the bits
    that show what's going on in our github organisation

--- git.sh
+++ git.sh
@@ -1,9 +1,6 @@
 rm -rf out
 mkdir out
 ./fetch_data.sh &> fetch_data.log || exit 1
-python plots.py || exit 1
-python make_csv.py || exit 1
-python speakers_kit.py || exit 1
 python make_html.py || exit 1
 cp favicon.png out/
 mv web web.1
--- github/fetch_milestones.py
+++ github/fetch_milestones.py
@@ -15,30 +15,31 @@ def fetch():
     except OSError:
         pass
 
-    r = requests.get('https://api.github.com/orgs/IATI/repos',auth=HTTPBasicAuth(user,password))
-    with open('data/github/repo.json', 'w') as fp:
-        fp.write(r.text.encode('utf-8'))
+    r = requests.get('https://api.github.com/orgs/opendataservices/repos',auth=HTTPBasicAuth(user,password))
+    print (r.text)
+    with open('data/github/repo.json', 'w', encoding='utf8') as fp:
+        fp.write(r.text)
     for repo in json.loads(r.text):
         milestones_url = repo['milestones_url'].replace('{/number}','')
         milestones_request = requests.get(milestones_url,auth=HTTPBasicAuth(user,password))
-        with open('data/github/milestones/{0}.json'.format(repo['name']), 'w') as fp:
-            fp.write(milestones_request.text.encode('utf-8'))
+        with open('data/github/milestones/{0}.json'.format(repo['name']), 'w', encoding='utf8') as fp:
+            fp.write(milestones_request.text)
 
         issues_url = repo['issues_url'].replace('{/number}','')
         issues_no_milestone_request = requests.get(issues_url, params={'milestone':'none'},auth=HTTPBasicAuth(user,password))
-        with open('data/github/issues_no_milestone/{0}.json'.format(repo['name']), 'w') as fp:
-            fp.write(issues_no_milestone_request.text.encode('utf-8'))
+        with open('data/github/issues_no_milestone/{0}.json'.format(repo['name']), 'w', encoding='utf8') as fp:
+            fp.write(issues_no_milestone_request.text)
         
         open_issues = repo['open_issues']
         if open_issues == 0:
-            with open('data/github/no_open_issues/{0}.json'.format(repo['name']), 'w') as fp:
+            with open('data/github/no_open_issues/{0}.json'.format(repo['name']), 'w', encoding='utf8') as fp:
                 fp.write('0')
      
         milestones_closed_url = repo['milestones_url'].replace('{/number}','') + '?state=closed'
         milestones_request = requests.get(milestones_closed_url,auth=HTTPBasicAuth(user,password))
-        with open('data/github/milestones_closed/{0}.json'.format(repo['name']), 'w') as fp:
+        with open('data/github/milestones_closed/{0}.json'.format(repo['name']), 'w', encoding='utf8') as fp:
         #with open('data/github/milestones_closed/urls') as fp:
-            fp.write(milestones_request.text.encode('utf-8'))
+            fp.write(milestones_request.text)
             #fp.write(milestones_closed_url)
 
 
